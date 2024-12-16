d88b741b7bcaac443179c4c1f7c446bc62324111
adwenpa <joojo.abr@gmail.com>
2024-12-16 18:47:48 -0500

    Finished experimenting in index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@
 // that are not declared
 "use strict"
 
+/*
+const myName = "Joe";
+console.log(myName);
+console.log(typeof myName);
+
+const TEN = 10;
+console.log(TEN);
+console.log(typeof TEN);
+
+let isMentor = true;
+console.log(isMentor);
+console.log(typeof isMentor);
+
+isMentor = false;
+
+console.log(isMentor);
+console.log(typeof isMentor);
+*/
