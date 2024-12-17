e7b1180186d1ae2c861812f5fccc3eeb4aa0f103
adwenpa <joojo.abr@gmail.com>
2024-12-16 19:30:16 -0500

    Created variables and updated all the empty functions

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,39 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Be thankful";
+const currentYear = 2024;
+let willMeetResolution = true;
 
 function updateYear() {
     // Update this function
+    yearElement.innerText = currentYear + 1;
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
+    willMeetResolutionElement.innerText = willMeetResolution;
 
 }
 
 function render() {
     // Update this function
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
+
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
+    render();
 
 })
